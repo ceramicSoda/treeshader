e624ee4dcc8eb06d44d337621e61dbe238ebd1ff
Ceramic Soda <dmch.d89@gmail.com>
2023-07-20 18:14:02 +0300

    Object space shader

--- leavesShader.js
+++ leavesShader.js
@@ -1,36 +1,50 @@
 export const leavesVS = /*glsl*/`
+    uniform vec3 uBoxMin;
+    uniform vec3 uBoxSize;
     varying vec3 vWorldPos; 
+    varying vec3 vObjectPos; 
     varying vec3 vNormal; 
     varying vec3 vViewPosition;
     void main(){
         vNormal = normalMatrix * mat3(instanceMatrix) * normalize(normal); 
         gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(position, 1.);
         vWorldPos = vec3(modelMatrix * instanceMatrix * vec4(position, 1.));
+        vObjectPos = (vWorldPos - uBoxMin) / uBoxSize; 
         vViewPosition = -normalize((modelViewMatrix * vec4(position, 1.)).xyz);
     }
 `
 export const leavesFS = /*glsl*/`
     #include <common> 
     #include <lights_pars_begin>
+    varying vec3 vObjectPos; 
     varying vec3 vWorldPos;
     varying vec3 vNormal; 
     varying vec3 vViewPosition;
     uniform vec3    uColor;
     uniform float   uState; 
 
+    vec3 getPosColors(){
+        vec3 p = vec3(pow((1. - vNormal.y - 0.4), 4.)) * uColor;
+        return p;
+    }
+
     vec3 getDiffuse(){
-        vec3 l = vec3(uColor);
-        float intensity = dot(directionalLights[0].direction, vNormal);
-        intensity = smoothstep(0.5, 0.7, intensity);
-        l = vec3(   uColor.r + (intensity / 10.0),
-                    uColor.g + (intensity / 6.0), 
-                    uColor.b + (intensity / 18.0)); 
+        vec3 l;
+        float intensity;
+        for (int i = 0; i < directionalLights.length(); i++){
+            intensity = dot(directionalLights[i].direction, vNormal);
+            intensity = pow(smoothstep(0.75, 1., intensity), 0.3);
+            //l = l + vec3(intensity / 10.0, intensity / 6.0, intensity / 18.0); 
+            l = l + intensity * (uColor * directionalLights[i].color) / 2.; 
+        }
         return l;
     }
 
     void main(){
         vec4 c = vec4(uColor, 1.0);
-        c.xyz = getDiffuse();
+        c = vec4(c.xyz + getDiffuse(), c.w);
+        c = vec4(c.xyz + getPosColors(), c.w);
         gl_FragColor = vec4( pow(c.xyz,vec3(0.454545)), c.w );
+        gl_FragColor = vec4(vObjectPos * vNormal, 1.);
     }
 `
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { leavesVS, leavesFS } from "./leavesShader.js"
 import './style.css';
-//-----------------------------THREE 
+// GENERAL DEFINITIONS
 const scene = new THREE.Scene();
 const test = new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshPhongMaterial({color: 0xfa8023, shininess: 100, specular: 100}));
 const loader = new GLTFLoader();
@@ -12,69 +12,68 @@ const renderer = new THREE.WebGLRenderer({alpha: true});
 const controls = new OrbitControls(camera, renderer.domElement);
 const dlight01 = new THREE.DirectionalLight(0xcccccc, 0.8);
 const tree = {group: new THREE.Group()};
-//-----------------------------MATERIALS
+// MATERIALS
 const leavesMat = new THREE.ShaderMaterial({
   lights: true,
   side: THREE.DoubleSide,
   uniforms: {
     ...THREE.UniformsLib.lights,
     uTime: {value: 0.},
-    uBoxMin: {value: new THREE.Vector3(0,0,0)},
-    uBoxMax: {value: new THREE.Vector3(0,0,0)},
     uColor: {value: new THREE.Color(0xe5752b)},
+    uBoxMin: {value: new THREE.Vector3(0,0,0)},
+    uBoxSize: {value: new THREE.Vector3(10,10,10)},
   },
   vertexShader: leavesVS,
   fragmentShader: leavesFS,
 })
-//-----------------------------GLTF
+// GLTF LOADING 
 loader.loadAsync("assets/tree_flat.glb")
 .catch(err => console.error(err))
 .then(obj => {
   tree.pole = obj.scene.getObjectByName("Pole");
   tree.pole.material = new THREE.MeshBasicMaterial({color: 0xc4a5b4});
+  // Each vertex of crown mesh will be a leaf
+  // Crown mesh won't be visible in scene
   tree.crown = obj.scene.getObjectByName("Leaves");
+  // For object space shader
+  tree.bbox = new THREE.Box3().setFromObject(tree.crown);
+  leavesMat.uniforms.uBoxMin.value.copy(tree.bbox.min); 
+  leavesMat.uniforms.uBoxSize.value.copy(tree.bbox.getSize(new THREE.Vector3())); 
+  
   tree.leavesCount = tree.crown.geometry.attributes.position.count;
   tree.leafGeometry = obj.scene.getObjectByName("Leaf").geometry; 
-  console.log(tree.leavesCount);
-  tree.leaves = new THREE.InstancedMesh(tree.leafGeometry, 
-                                        leavesMat,
-                                        tree.leavesCount); 
+  tree.leaves = new THREE.InstancedMesh(tree.leafGeometry, leavesMat, tree.leavesCount); 
   const dummy = new THREE.Object3D();
   for (let i = 0; i < tree.leavesCount; i++) { 
     dummy.position.x = tree.crown.geometry.attributes.position.array[i*3];
     dummy.position.y = tree.crown.geometry.attributes.position.array[i*3+1];
     dummy.position.z = tree.crown.geometry.attributes.position.array[i*3+2];
-    
-    dummy.rotation.x = Math.random() * 0.1;
-    dummy.rotation.y = Math.random() * 0.1;
-    dummy.rotation.z = Math.random() * 0.1;
-
     dummy.lookAt(dummy.position.x + tree.crown.geometry.attributes.normal.array[i*3],
                  dummy.position.y + tree.crown.geometry.attributes.normal.array[i*3+1],
                  dummy.position.z + tree.crown.geometry.attributes.normal.array[i*3+2]);
-    dummy.scale.x = (Math.random() * 0.4 + 0.6);
-    dummy.scale.y = (Math.random() * 0.4 + 0.6);
-    dummy.scale.z = (Math.random() * 0.4 + 0.6);
+    dummy.scale.x = (Math.random() * 0.2 + 0.8);
+    dummy.scale.y = (Math.random() * 0.2 + 0.8);
+    dummy.scale.z = (Math.random() * 0.2 + 0.8);
     dummy.updateMatrix();
     tree.leaves.setMatrixAt(i, dummy.matrix);
   }
   tree.group.add(tree.pole, tree.leaves);
 })
-//-----------------------------INIT
+// INIT
 document.body.appendChild(renderer.domElement); 
 renderer.setAnimationLoop(animate);
 renderer.setSize(window.innerWidth, window.innerHeight);
-dlight01.position.set(4,2,0);
-dlight01.lookAt(0,0,0);
+dlight01.position.set(3,8,-3);
+dlight01.lookAt(0,2.4,0);
 camera.position.set(12,4,0);
 controls.target = new THREE.Vector3(0,2.4,0);
 scene.add(dlight01, tree.group);
-//-----------------------------LOOP
+// MAIN LOOP
 function animate () {
   controls.update(); 
   renderer.render(scene, camera); 
 }
-//------------------------------RESIZE
+// EVENTS
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
